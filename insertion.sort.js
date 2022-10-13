edf11cd35a11d8a79a766ff2d8250a06ec4a6b75
Ole Larsen <39528957+ole-larsen@users.noreply.github.com>
2022-10-13 17:52:13 +0530

    Update insertion.sort.js

--- insertion.sort.js
+++ insertion.sort.js
@@ -1,17 +1,8 @@
 export default function insertionSort(arr) {
-  let index, value;
-
   for (let i = 1; i < arr.length; i++) {
-    value = arr[i];
-    index = i;
-    
-    while (index > 0 && arr[index - 1] > value) {
-      arr[index] = arr[index - 1];
-      index = index - 1;
+    while (arr[i - 1] > arr[i]) {
+      arr[i] = arr[i - 1];
     }
-    
-    arr[index] = value;
   }
-  
   return arr;
 }
