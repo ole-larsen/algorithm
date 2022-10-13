b833b30cec41a39accbfde10f6fc8247deb5b7dd
Ole Larsen <39528957+ole-larsen@users.noreply.github.com>
2022-10-13 20:26:02 +0530

    Update selection.sort.js

--- selection.sort.js
+++ selection.sort.js
@@ -1,22 +1,27 @@
 function findSmallest(arr) {
   let min = arr[0];
   let minIndex = 0;
+  
   for (let i = 1; i < arr.length; i++) {
     if (min > arr[i]) {
       min = arr[i];
       minIndex = i;
     }
   }
+  
   return minIndex;
 }
 
 export default function selectionSort(arr) {
   const sorted = [];
+  
   while (arr.length) {
     const smallestIndex = findSmallest(arr);
     const smallest = arr[smallestIndex];
+  
     sorted.push(smallest);
     arr.splice(smallestIndex, 1);
   }
+  
   return sorted;
 }
