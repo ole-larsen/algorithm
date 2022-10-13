45b5e23f73e5a53a015e7b1d25a64eba6a094231
Ole Larsen <39528957+ole-larsen@users.noreply.github.com>
2022-10-13 16:03:09 +0530

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,8 @@
+export default function fibonacci(n) {
+  if (n === 0) return 0;
+  const arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
